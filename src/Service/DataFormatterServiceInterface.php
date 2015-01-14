81f01f945fff4e314b3f7ceaf21dead23e1ed11e
Clement Guinet <guinet.clement@gmail.com>
2015-01-14 16:12:54 +0100

    correctif declaration methods Interfaces

--- src/Service/DataFormatterServiceInterface.php
+++ src/Service/DataFormatterServiceInterface.php
@@ -8,4 +8,6 @@ namespace Lequipe\Service;
  */
 interface DataFormatterServiceInterface {
     public function getFormat();
+    
+    public function setFormatParams();
 }
--- src/Service/Video/MapperVideoInterface.php
+++ src/Service/Video/MapperVideoInterface.php
@@ -3,10 +3,17 @@
 namespace Lequipe\Service\Video;
 
 use Lequipe\Entity\Video;
+use Lequipe\Entity\TypeVideo;
 /**
  *
  * @author cguinet
  */
 interface MapperVideoInterface {
     public function populateVideo(Video $vid, $datas);
+    
+    public function getVideos($datas);
+            
+    public function populateTypeVideo(TypeVideo $typeVid, $datas);
+    
+    public function getTypeVideos($datas);
 }

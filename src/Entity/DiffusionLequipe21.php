2d8cd0b582f5735e430fedc64ec9c8de09f43d3d
Mikhail MARYNICH <mmarynich-ext@lequipe.fr>
2016-03-09 17:04:45 +0100

    création d'une entité "DiffusionLequipe21"

---
+++ src/Entity/DiffusionLequipe21.php
@@ -0,0 +1,213 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mmarynich
+ * Date: 08/03/16
+ * Time: 18:05
+ */
+
+namespace Lequipe\Entity;
+
+
+class DiffusionLequipe21
+{
+    private $idDiffusion;
+    private $dateDiffusion;
+    private $heureDiffusion;
+    private $titleDiffusion;
+    private $descriptifDiffusion;
+    private $dureeDiffusion;
+    private $typeDiffusion;
+    private $idEmission;
+    private $dureeEmission;
+    private $nomEmission;
+    private $titleEmission;
+
+    /**
+     * @return mixed
+     */
+    public function getIdDiffusion()
+    {
+        return $this->idDiffusion;
+    }
+
+    /**
+     * @param mixed $idDiffusion
+     */
+    public function setIdDiffusion($idDiffusion)
+    {
+        $this->idDiffusion = $idDiffusion;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateDiffusion()
+    {
+        return $this->dateDiffusion;
+    }
+
+    /**
+     * @param mixed $dateDiffusion
+     */
+    public function setDateDiffusion($dateDiffusion)
+    {
+        $this->dateDiffusion = $dateDiffusion;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHeureDiffusion()
+    {
+        return $this->heureDiffusion;
+    }
+
+    /**
+     * @param mixed $heureDiffusion
+     */
+    public function setHeureDiffusion($heureDiffusion)
+    {
+        $this->heureDiffusion = $heureDiffusion;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitleDiffusion()
+    {
+        return $this->titleDiffusion;
+    }
+
+    /**
+     * @param mixed $titleDiffusion
+     */
+    public function setTitleDiffusion($titleDiffusion)
+    {
+        $this->titleDiffusion = $titleDiffusion;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescriptifDiffusion()
+    {
+        return $this->descriptifDiffusion;
+    }
+
+    /**
+     * @param mixed $descriptifDiffusion
+     */
+    public function setDescriptifDiffusion($descriptifDiffusion)
+    {
+        $this->descriptifDiffusion = $descriptifDiffusion;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDureeDiffusion()
+    {
+        return $this->dureeDiffusion;
+    }
+
+    /**
+     * @param mixed $dureeDiffusion
+     */
+    public function setDureeDiffusion($dureeDiffusion)
+    {
+        $this->dureeDiffusion = $dureeDiffusion;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTypeDiffusion()
+    {
+        return $this->typeDiffusion;
+    }
+
+    /**
+     * @param mixed $typeDiffusion
+     */
+    public function setTypeDiffusion($typeDiffusion)
+    {
+        $this->typeDiffusion = $typeDiffusion;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdEmission()
+    {
+        return $this->idEmission;
+    }
+
+    /**
+     * @param mixed $idEmission
+     */
+    public function setIdEmission($idEmission)
+    {
+        $this->idEmission = $idEmission;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDureeEmission()
+    {
+        return $this->dureeEmission;
+    }
+
+    /**
+     * @param mixed $dureeEmission
+     */
+    public function setDureeEmission($dureeEmission)
+    {
+        $this->dureeEmission = $dureeEmission;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNomEmission()
+    {
+        return $this->nomEmission;
+    }
+
+    /**
+     * @param mixed $nomEmission
+     */
+    public function setNomEmission($nomEmission)
+    {
+        $this->nomEmission = $nomEmission;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitleEmission()
+    {
+        return $this->titleEmission;
+    }
+
+    /**
+     * @param mixed $titleEmission
+     */
+    public function setTitleEmission($titleEmission)
+    {
+        $this->titleEmission = $titleEmission;
+        return $this;
+    }
+
+}
